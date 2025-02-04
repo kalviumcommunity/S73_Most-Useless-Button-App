28b8ca1ed5b91329ea5829a0678ceece30c2d762
VRAJPATEL621204 <vrajpatel621204@gmail.com>
2025-02-04 16:27:57 +0530

    ASAP 4 completed

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
+
+//Importing and creating instances of the packages , and defining the port number.
 const express = require('express');
 const app = express();
 const port = 3000;
 
+
+
+// Define a GET endpoint at '/ping' that responds with 'pong'
 app.get('/ping', (req, res) => {
     res.send('pong');
 });
+
+
+// Start the server and listen on the defined port
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
